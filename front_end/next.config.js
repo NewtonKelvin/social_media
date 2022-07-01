4214acdd7801f436bc2a1e7b17463316e74f730a
Kelvin Newton <kelvinnewtonsm@gmail.com>
2022-07-01 01:18:57 -0300

    Hide AWS Keys

--- front_end/next.config.js
+++ front_end/next.config.js
@@ -27,11 +27,6 @@ const nextConfig = {
   images: {
     domains: ['s3.amazonaws.com', '192.168.15.81'],
     minimumCacheTTL: 1 * 60,
-  },
-  env: {
-    ENVIRONMENT: "development",
-    AWS_S3_LINK: "https://s3.amazonaws.com/kn-social.media",
-    BACK_END: "http://192.168.15.81:3001/v1"
   }
 }
 
